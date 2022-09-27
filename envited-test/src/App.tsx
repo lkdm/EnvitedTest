4f9a331bc0463cd523d2555251f1a5e2a4dd2636
Luke <44519853+lkdm@users.noreply.github.com>
2022-09-27 14:07:10 +1000

    chore: create pages.

--- envited-test/src/App.tsx
+++ envited-test/src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Create from './pages/CreatePage';
+import Index from './pages/IndexPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" />
-        <Route path="create" />
-        <Route path="event" />
+        <Route path="/" element={<Index />} />
+        <Route path="create" element={<Create />} />
+        <Route path="event"  element={<Index />} />
       </Routes>
     </div>
   );
